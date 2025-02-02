80435024f1d0c62c14b485d0ce812c589ed85052
maheshbasnet089 <basnetmanish089@gmail.com>
2025-02-02 20:32:34 +0545

    add category

---
+++ src/app/api/category/category.controller.ts
@@ -0,0 +1,54 @@
+import dbConnect from "@/database/connection"
+import Category from "@/database/models/category.schema"
+
+
+export async function createCategory(req:Request){
+    try {
+        await dbConnect()
+        const {name,description} =  await req.json()
+        // already exist or not 
+        const existingCategory = await Category.findOne({name : name})
+        if(existingCategory){
+            return Response.json({
+                message : "Category already existed with that name !!"
+            },{status:400})
+        }
+        await Category.create({
+            name,
+            description
+        })
+        return Response.json({
+            message : "Category created successfully !"
+        },{
+            status : 201
+        })
+    } catch (error) {
+        console.log(error)
+        return Response.json({
+            messsage : "Something went wrong!!"
+        },{status:500})
+    }
+}
+
+export async function getCategories(){
+  try {
+    await dbConnect()
+    const categories = await Category.find() // returns array , findOne --> re ob, findById --> re ob
+    if(categories.length === 0 ){
+        return Response.json({
+            message : "No categories found"
+        },{
+            status : 404
+        })
+    }
+    return Response.json({
+        message : "Category fetched successfully !!", 
+        data : categories
+    },{status : 200})
+  } catch (error) {
+    console.log(error)
+    return Response.json({
+        messsage : "Something went wrong!!"
+    },{status:500})
+  }
+}
---
+++ src/app/api/category/route.ts
@@ -0,0 +1,11 @@
+import { createCategory, getCategories } from "./category.controller";
+
+
+export async function POST(req:Request){
+    return createCategory(req)
+
+}
+
+export async function GET(){
+     return getCategories()
+}
